a2ab50541fb8f6cdd062a615be921739ce881010
Gabriel Ribeiro de Araújo <gableisure@sempreceub.com>
2022-07-05 14:57:39 -0300

    push heroku

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,8 @@
 
 export const environment = {
   production: false,
-  baseUrlApi: "http://localhost:3334",
+  baseUrlApi: "https://dev-api-sgd.herokuapp.com/",
+  
 
   teds: {
     tedsTheme: ['#5ac7aa', '#bfe3c3', '#dbedc2', '#a1c1be', '#bbdec6', '#dad8a7',  '#edffb3', '#f3f4e5', '#f7f3cf',  '#e9f3fc', '#e2e3d9', '#fff2d4']
